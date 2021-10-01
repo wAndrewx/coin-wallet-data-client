78e253ded1504b00a59bb13543a5807ea14c2319
Andrew Huynh <andrew.huynh17@gmail.com>
2021-10-01 14:44:57 -0400

    update host for api

--- utils/nativeAPI.ts
+++ utils/nativeAPI.ts
@@ -1,23 +1,19 @@
 import axios from "axios"
 
-const connection = 'http://localhost:8080'
-
 const getDaily = async () => {
-    let res = await axios.get(`${connection}/coin/daily`)
-    // console.log(res.data)
+    let res = await axios.get(`${process.env.API_HOST}/coin/daily`)
     return res.data.query
 }
 
 const getTotal = async () => {
-    let res = await axios.get(`${connection}/coin/total`)
-    console.log(res.data)
+    let res = await axios.get(`${process.env.API_HOST}/coin/total`)
     return res.data.query
 }
 
 const incrementCoin = async (ticker: string, token: string) => {
     if (ticker && token) {
         try {
-            let res = await axios.patch(`${connection}/coin/total`, {}, { params: { ticker: ticker, token: token } })
+            let res = await axios.patch(`${process.env.API_HOST}/coin/total`, {}, { params: { ticker: ticker, token: token } })
 
         } catch (error) {
             console.log(error)
