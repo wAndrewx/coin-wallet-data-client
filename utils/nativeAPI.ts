3089685da2bfb1c42a92f88b35886badaa9fce8d
Andrew Huynh <andrew.huynh17@gmail.com>
2021-10-01 14:55:45 -0400

    change env name

--- utils/nativeAPI.ts
+++ utils/nativeAPI.ts
@@ -1,19 +1,19 @@
 import axios from "axios"
 
 const getDaily = async () => {
-    let res = await axios.get(`${process.env.API_HOST}/coin/daily`)
+    let res = await axios.get(`${process.env.NEXT_PUBLIC_API_HOST}/coin/daily`)
     return res.data.query
 }
 
 const getTotal = async () => {
-    let res = await axios.get(`${process.env.API_HOST}/coin/total`)
+    let res = await axios.get(`${process.env.NEXT_PUBLIC_API_HOST}/coin/total`)
     return res.data.query
 }
 
 const incrementCoin = async (ticker: string, token: string) => {
     if (ticker && token) {
         try {
-            let res = await axios.patch(`${process.env.API_HOST}/coin/total`, {}, { params: { ticker: ticker, token: token } })
+            let res = await axios.patch(`${process.env.NEXT_PUBLIC_API_HOST}/coin/total`, {}, { params: { ticker: ticker, token: token } })
 
         } catch (error) {
             console.log(error)
